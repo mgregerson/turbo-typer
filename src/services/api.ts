7fa48699dd7e610d97b201b797e5ad00d4d80104
mgregerson <mgregerson4321@gmail.com>
2023-07-28 13:23:58 -0400

    FEAT: TypingApi class for handling API requests:
    This commit introduces the TypingApi class, which provides a set of static methods for making API requests to a backend server. The class utilizes Axios for handling HTTP requests and defines methods for user authentication (register and login) and fetching a random typing test based on difficulty level. The class also implements token-based authentication by storing the users token in a static property, enabling subsequent API calls with the authorization header when required. This addition allows for easy integration of API functionalities in the application.

---
+++ src/services/api.ts
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080";
+
+type LoginResponse = {
+  username: string;
+  id: string;
+  email: string;
+  token: string;
+};
+
+class TypingApi {
+  static token: string = "";
+
+  static async request<T>(
+    endpoint: string,
+    method: "get" | "post" | "patch" | "delete" = "get",
+    data: any = {}
+  ): Promise<T> {
+    console.debug("API Call:", endpoint, data, method, this.token);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = this.token
+      ? { Authorization: `Bearer ${this.token}` }
+      : undefined;
+    const params = method === "get" ? data : {};
+
+    try {
+      const response: any = await axios({
+        url,
+        method,
+        data,
+        params,
+        headers,
+      });
+      return response.data;
+    } catch (err: any) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  static async register(email: string, username: string, password: string) {
+    const data = { email, username, password };
+    const res: LoginResponse = await this.request<LoginResponse>(
+      "auth/register",
+      "post",
+      data
+    );
+    this.token = res.token;
+    return res;
+  }
+
+  static async login(email: string, password: string) {
+    const data = { email, password };
+    const res: LoginResponse = await this.request<LoginResponse>(
+      "auth/login",
+      "post",
+      data
+    );
+    localStorage.setItem("USER-AUTH", res.token); // Store the token in local storage
+    return res;
+  }
+
+  static async getRandomTypingTestByDifficulty(difficulty: string) {
+    const res = await this.request<any>(
+      `typingtests/${difficulty}/random`,
+      "get"
+    );
+    console.log(res);
+    return res;
+  }
+}
+
+export default TypingApi;
