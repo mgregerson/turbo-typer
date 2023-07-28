35c02821ff87c8f77b1424a1a6830a95b2b3dd80
mgregerson <mgregerson4321@gmail.com>
2023-07-28 13:21:02 -0400

    Refactor: Update route elements in RoutesList
    This commit refactors the route elements in the RoutesList Component. It replaces the component imports with relevant paths and removes unnecessary imports for pages that are not currently being used.

---
+++ src/routes/RoutesList.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import App from "../App";
+import LoginForm from "../components/forms/LoginForm";
+import RegisterForm from "../components/forms/RegisterForm";
+import NewTestForm from "../components/forms/NewTestForm";
+import { TypingTest } from "../pages/TypingTest";
+
+function RoutesList(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/newgame" element={<NewTestForm />} />
+      <Route path="/typingtest/:id" element={<TypingTest />} />
+      <Route path="/leaderboard" element={<App />} />
+      <Route path="/about" element={<App />} />
+      <Route path="/profile" element={<App />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="*" element={<App />} />
+    </Routes>
+  );
+}
+
+export default RoutesList;
