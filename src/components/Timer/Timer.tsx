d5e1710a5128183b67ac9a7ad8249585ab6ce754
mgregerson <mgregerson4321@gmail.com>
2023-08-16 13:47:03 -0400

    Updated Timer to handle timer counting down logic and remove state dependencies in TypingTest component.

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from "react";
 
 interface TimerProps {
   minutes: string;
+  handleTimerExpired: () => void;
 }
 
-function Timer({ minutes }: TimerProps) {
-  const [remainingTime, setRemainingTime] = useState(parseInt(minutes) * 60); // Convert minutes to seconds
+function Timer({ minutes, handleTimerExpired }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(parseInt(minutes) * 60);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (remainingTime > 0) {
         setRemainingTime((prevTime) => prevTime - 1);
       } else {
-        clearInterval(interval); // Stop the timer when it reaches 0
+        clearInterval(interval);
+        handleTimerExpired(); // Notify the parent when the timer expires
       }
-    }, 1000); // Update every 1 second
+    }, 1000);
 
     return () => {
-      clearInterval(interval); // Clear the interval on component unmount
+      clearInterval(interval);
     };
   }, [remainingTime]);
 
