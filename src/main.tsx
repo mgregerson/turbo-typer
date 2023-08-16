4c62d0459aa4c1d5122ed4b48810c9e616a30bb1
mgregerson <mgregerson4321@gmail.com>
2023-08-16 17:04:21 -0400

    Added Google OAuth to prepare for Google SSO.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import RoutesList from "./routes/RoutesList.tsx";
 import Navbar from "./components/Navbar.tsx";
 import { StoryProvider } from "./context/StoryContext";
+import { ResultsProvider } from "./context/ResultsContext.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./styles/index.css";
 
 import "@fontsource/roboto/400.css";
@@ -11,13 +13,19 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const GOOGLE_AUTH_ID = import.meta.env.VITE_GOOGLE_AUTH_ID;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <StoryProvider>
-        <Navbar />
-        <RoutesList />
-      </StoryProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={String(GOOGLE_AUTH_ID)}>
+      <BrowserRouter>
+        <StoryProvider>
+          <ResultsProvider>
+            <Navbar />
+            <RoutesList />
+          </ResultsProvider>
+        </StoryProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
