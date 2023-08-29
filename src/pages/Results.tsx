1b97c6f094937c1e6f85e38ecd129b53d2cdc555
mgregerson <mgregerson4321@gmail.com>
2023-08-29 13:12:20 -0400

    Added Results component - renders past and present results in separate tables

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,13 +1,51 @@
 import { ResultsContext } from "../context/ResultsContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import TypingApi from "../services/api";
 import ResultsTable from "../components/tables/ResultsTable";
+import PastResultsTable, {
+  PastResults,
+} from "../components/tables/PastResultsTable";
 
-export function ResultsCard() {}
+async function getPastResults(difficulty: string, username: string) {
+  const response = await TypingApi.getScoresByUserAndDifficulty(
+    difficulty,
+    username
+  );
+  return response;
+}
 
 export default function Results() {
   const { results } = useContext(ResultsContext);
+  const [pastResults, setPastResults] = useState<PastResults[] | null>(null);
 
-  if (results) {
-    return <ResultsTable results={results} />;
-  }
+  useEffect(() => {
+    async function fetchPastResults() {
+      if (results !== null) {
+        const username = localStorage.getItem("username");
+
+        if (typeof username === "string") {
+          const resultsData = await getPastResults(
+            results.difficulty,
+            username
+          );
+          setPastResults(resultsData);
+        } else {
+          console.log("Username is not a valid string.");
+        }
+      }
+    }
+
+    fetchPastResults();
+  }, []);
+
+  console.log(pastResults, "pastResults in Results.tsx");
+
+  return (
+    <>
+      {results && <ResultsTable results={results} />}
+      {pastResults && pastResults.length > 0 && (
+        <PastResultsTable results={pastResults} />
+      )}
+    </>
+  );
 }
